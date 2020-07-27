03101740cb4e4fe7ed5f277ef4d027495972e848
igorhristov <igorhristov@sourcemxacademy.mk>
2020-07-27 21:36:09 +0200

    add engines for heroku and images for data

--- src/components/services-card/services-card.jsx
+++ src/components/services-card/services-card.jsx
@@ -3,16 +3,16 @@ import { Card, Button } from 'react-bootstrap';
 import './services-card.styles.css';
 
 const ServicesCard = ({ imgUrl, title, body }) => (
-    <Card style={{ borderColor: '#EC4090' }}>
+    <Card className='' style={{ borderColor: 'transparent', boxShadow: '1px 1px 3px  #EC4090' }}>
         <Card.Img variant='bottom' src={require(`../../../assets/images/${imgUrl}`)} width='400' height='170' />
         <Card.Header
             className='d-flex align-items-center justify-content-center text-center font-weight-bold p-2'
-            style={{ minHeight: '87px', background: 'none', borderColor: 'transparent', color:"#EC4090" }}
+            style={{ minHeight: '87px', background: 'none', borderColor: 'transparent', color: '#EC4090' }}
         >
             {title}
         </Card.Header>
-        <Card.Body>
-            <Card.Text>{body[0].slice(0, 135) + '...'}</Card.Text>
+        <Card.Body className='mt-0 pt-0'>
+            <Card.Text className='mt-0 pt-0'>{body[0].slice(0, 135) + '...'}</Card.Text>
         </Card.Body>
         <Card.Footer style={{ background: 'none', borderColor: 'transparent' }}>
             <Button variant='outline-danger' className='ServicesButton '>
